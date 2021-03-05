a08c646d19a978ad721387c58cef733105076a3e
Yamini98777 <80026410+Yamini98777@users.noreply.github.com>
2021-03-05 13:55:14 +0530

    Add files via upload

---
+++ welcomeapp-lib/src/test/java/welcomeapp/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package welcomeapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+	@Test
+	void testGetName() {
+		assertEquals("yamini", HelloWorld.getName());
+	}
+
+}
